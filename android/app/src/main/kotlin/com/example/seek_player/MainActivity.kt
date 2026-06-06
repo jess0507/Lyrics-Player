8f947f8b6f5f01030809a2d346dd89022420d43e
jess <merukoo0507@gmail.com>
2026-06-06 18:51:06 +0800

    MainActivity extend AudioServiceActivity for background audio support

--- android/app/src/main/kotlin/com/example/seek_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/seek_player/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.seek_player
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+// audio_service / just_audio_background 需要 Activity 繼承 AudioServiceActivity，
+// 以提供正確的 FlutterEngine 給背景播放服務。
+class MainActivity : AudioServiceActivity()
